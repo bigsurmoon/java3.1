e3651e95bb55c14cf6db7875bfa73e20cc726454
bigsurmoon <vinny4000@gmail.com>
2021-09-26 16:00:43 +0700

    init commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+      public static void main(String[] args) {
+          int ticketPrice = 10_000;
+          int amountMoneyForMile = 20;
+          int bonusMiles;
+          bonusMiles = ticketPrice / amountMoneyForMile;
+
+          System.out.println("Колличество бонусных миль: " + bonusMiles);
+
+
+
+
+
+        }
+
+
+
+
+    }
